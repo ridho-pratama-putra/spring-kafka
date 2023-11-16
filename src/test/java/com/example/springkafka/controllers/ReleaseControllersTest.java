1a7d3397198a29ff09a76519909bdd424c4c430f
ridho <ridhopratamavd@gmail.com>
2023-11-16 12:37:05 +0700

    change test to not start the server

--- src/test/java/com/example/springkafka/controllers/ReleaseControllersTest.java
+++ src/test/java/com/example/springkafka/controllers/ReleaseControllersTest.java
@@ -1,45 +1,46 @@
 package com.example.springkafka.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.springkafka.models.Release;
 import com.example.springkafka.services.RecommendationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@EnableAutoConfiguration
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 public class ReleaseControllersTest {
     
     @MockBean
     RecommendationService recommendationService;
 
-    @LocalServerPort
-	private int port;
-
 	@Autowired
-	private TestRestTemplate restTemplate;
+	private MockMvc mockMvc;
 
     @Test
-    void createAlbum_shouldCallService_whenCalled() {
+    void createAlbum_shouldCallService_whenCalled() throws Exception {
         Release build = Release.builder().build();
-        ResponseEntity<Release> serviceResponse = new ResponseEntity<>(Release.builder().build(), HttpStatus.CREATED);
+        ResponseEntity<Release> serviceResponse = new ResponseEntity<>(build, HttpStatus.CREATED);
         Mockito.when(recommendationService.produceTopicNewRelease(Mockito.any(Release.class))).thenReturn(serviceResponse);
         
-        ResponseEntity<Release> postForEntity = restTemplate
-            .postForEntity("/album", build, Release.class);
+        mockMvc
+            .perform(MockMvcRequestBuilders.post("/album")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(build)))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+        ;
         
         Mockito.verify(recommendationService, Mockito.times(1)).produceTopicNewRelease(Mockito.any());
-        assertEquals(serviceResponse.getStatusCode(), postForEntity.getStatusCode());
     }
 }
